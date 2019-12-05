ca172fd6b9c37bba2fcc48180909b34648218870
viscountfam <V3ctor42!>
2019-12-05 17:11:45 -0500

    I finished MVP and am now trying to do the stretch goals

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,10 +2,18 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+const cards = document.querySelector(".cards");
+axios.get("https://api.github.com/users/viscountfam")
+  .then(response => {
+    console.log(response);
+    cards.appendChild(CardMaker(response.data));
+  })
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
+
+
 
    Skip to Step 3.
 */
@@ -24,7 +32,21 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  'israelgonzalezb',
+  'johnnweke',
+  'aaronjan98',
+  'asbaden',
+  'damonbogich'
+];
+followersArray.forEach(item =>{
+ axios.get( `https://api.github.com/users/${item}`)
+  .then(res => {
+    console.log(item);
+    cards.appendChild(CardMaker(res.data));
+  });
+  
+})
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -45,7 +67,50 @@ const followersArray = [];
 </div>
 
 */
+function CardMaker(data) {
+  const
+    card = document.createElement('div'),
+    profilepic = document.createElement('img'),
+    cardinfo = document.createElement('div'),
+    name = document.createElement('h3'),
+    username = document.createElement('p'),
+    location = document.createElement('p'),
+    profile = document.createElement('p'),
+    address = document.createElement('a'),
+    followers = document.createElement('p'),
+    following = document.createElement('p'),
+    bio = document.createElement('p');
+  
 
+    //add text content
+    profilepic.src = data.avatar_url;
+    name.textContent = data.name;
+    username.textContent = `username: ${data.login}`;
+    location.textContent = `Location: ${data.location}`;
+    address.textContent = `GitHub: ${data.html_url}`;
+    followers.textContent = `Followers: ${data.followers}`;
+    following.textContent = `Following: ${data.following}`;
+    bio.textContent = data.bio;
+
+    //add classes
+    card.classList.add("card");
+    cardinfo.classList.add("card-info");
+    name.classList.add("name");
+    username.classList.add("username");
+
+    //append the children
+    card.appendChild(profilepic);
+    card.appendChild(cardinfo);
+    cardinfo.appendChild(name);
+    cardinfo.appendChild(username);
+    cardinfo.appendChild(location);
+    cardinfo.appendChild(profile);
+    profile.appendChild(address);
+    cardinfo.appendChild(followers);
+    cardinfo.appendChild(following);
+    
+    return card;
+}
 /* List of LS Instructors Github username's: 
   tetondan
   dustinmyers
